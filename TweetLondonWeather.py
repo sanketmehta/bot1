e2b64c51bc8973b9267f61e5bdf308907bf0ec5e
Sanket Mehta <sanket@ramp.digital>
2017-09-16 21:08:25 -0400

    Updated code to tweet London weather every minute

---
+++ TweetLondonWeather.py
@@ -0,0 +1,40 @@
+# Dependencies
+import tweepy
+import time
+import json
+import random
+import datetime
+import requests as req
+import os
+
+# Extract API key from env variables
+consumer_key = os.environ['twtconkey']
+consumer_secret = os.environ['twtconsec']
+access_token = os.environ['twtacctok']
+access_token_secret = os.environ['twtaccsec']
+owmkey = os.environ['owmkey']
+
+cityCountry = "london,uk"
+base_url = "http://api.openweathermap.org/data/2.5/weather?units=Imperial"
+
+# Setup Tweepy API Authentication
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+# Create function for tweeting
+def tweetMsg():
+
+    try:
+        upd_url = base_url + "&APPID=" + owmkey + "&q=" + cityCountry
+        weather_json = req.get(upd_url).json()
+        api.update_status("London Weather as of %s: %s F" %
+        (datetime.datetime.now().strftime("%I:%M %p"),
+        weather_json["main"]["temp"]))
+    except Exception as e:
+        print("Exception occured: ", e)
+
+# Tweet london weather every minute
+while(True):
+    tweetMsg()
+    time.sleep(60)
